68b4e8010f4a21110d353d1859b02bf2b133c983
kingyo <kingyo.no.esa1192@gmail.com>
2021-06-19 01:04:32 +0900

    Update tb_top.v

--- Tang-Nano/sim/rtl/tb_top.v
+++ Tang-Nano/sim/rtl/tb_top.v
@@ -3,10 +3,10 @@
 module tb_top ();
     parameter MCO_HZ = 24000000;
 
-	reg		mco = 1'b0;
-    reg		res_n = 1'b1;
-	
-	// LCD
+    reg             mco = 1'b0;
+    reg             res_n = 1'b1;
+
+    // LCD
     wire    [7:0]   lcd_data;
     wire            lcd_wr;
     wire            lcd_dc;
